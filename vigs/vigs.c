51947e7171f9bac8b10103bdc8aab0b1b00c6f4c
Max Zinkus <mzinkus@calpoly.edu>
2016-04-21 22:29:41 -0700

    Finish encryption/decryption

--- vigs/vigs.c
+++ vigs/vigs.c
@@ -5,37 +5,19 @@
 #include <ctype.h>
 #include <sys/stat.h>
 
-#define UPPER_OFFSET 'A'
-#define LOWER_OFFSET 'a'
+#define OFFSET 'A'
 #define ENCRYPT 0
 #define DECRYPT 1
+#define ALPHABET_SIZE 26
 
 void usage() {
    printf("Usage: vigs [ -v ] [ -d ] key [ infile [ outfile ] ]");
 }
 
-int process_args(char **argv, int *verbose, int *mode, char **key, char **infile, char **outfile) {
-   while (*++argv) {
-      if (!strcmp(*argv, "-v")) {
-         *verbose = 1;
-      }
-      else if (!strcmp(*argv, "-d")) {
-         *mode = DECRYPT;
-      }
-      else if (!*key) {
-         *key = *argv;
-      }
-      else if (!*infile) {
-         *infile = *argv;
-      }
-      else if (!*outfile) {
-         *outfile = *argv;
-      }
-      else {
-         return 1;
-      }
-   }
-   return 0;
+int mod(int a, int b)
+{
+   int r = a % b;
+   return r < 0 ? r + b : r;
 }
 
 int main(int argc, char **argv) {
@@ -43,37 +25,62 @@ int main(int argc, char **argv) {
    int verbose = 0, mode = ENCRYPT;
    int infd = 0, outfd = 0;
    int keylen = 0, i = 0;
-   char x, c, *key = NULL, *infile = NULL, *outfile = NULL;
+   unsigned char x, c;
+   char *key = NULL, *infile = NULL, *outfile = NULL;
 
-   if (process_args(argv, &verbose, &mode, &key, &infile, &outfile)) {
-      usage();
-      return 1;
-   }
-
-   if (!key) {
-      fprintf(stderr, "No key specified\n");
-      return 1;
-   }
-
-   while (key[keylen]) {
-      if (!isalpha(key[keylen++])) {
-         fprintf(stderr, "Key must be alphabetic\n");
+   while (*++argv) {
+      if (!strcmp(*argv, "-v")) {
+         verbose = 1;
+      }
+      else if (!strcmp(*argv, "-d")) {
+         mode = DECRYPT;
+      }
+      else if (!key) {
+         key = *argv;
+      }
+      else if (!infile) {
+         infile = *argv;
+      }
+      else if (!outfile) {
+         outfile = *argv;
+      }
+      else {
+         usage();
          return 1;
       }
    }
 
-   printf("V: %d\n", verbose);
-   printf("D: %d\n", mode);
-   printf("KEY: %s\n", key);
-   if (infile)
-      printf("IN: %s\n", infile);
-   if (outfile)
-      printf("OUT: %s\n", outfile);
+   if (!key) {
+      fprintf(stderr, "No key specified\n");
+      usage();
+      return 1;
+   }
+
+   while (key[keylen]) {
+      if (!isalpha(key[keylen])) {
+         fprintf(stderr, "Key must be alphabetic\n");
+         usage();
+         return 1;
+      }
+      key[keylen] = toupper(key[keylen]);
+      key[keylen] = key[keylen] - OFFSET;
+      keylen++;
+   }
+
+   if (verbose) {
+      printf("V: %d\n", verbose);
+      printf("D: %d\n", mode);
+      if (infile)
+         printf("I: %s\n", infile);
+      if (outfile)
+         printf("O: %s\n", outfile);
+   }
 
    if (infile) {
       infd = open(infile, O_RDONLY);
       if (infd == -1) {
          fprintf(stderr, "Error reading from file %s\n", infile);
+         usage();
          return 1;
       }
       dup2(infd, 0);
@@ -83,32 +90,24 @@ int main(int argc, char **argv) {
       outfd = open(outfile, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
       if (outfd == -1) {
          fprintf(stderr, "Error writing to file %s\n", outfile);
+         usage();
          return 1;
       }
       dup2(outfd, 1);
    }
 
-   for (i = 0; scanf("%c", &c) != EOF; i++) {
-      x = key[i % keylen];
+   for (i = 0; scanf("%c", &c) != EOF;) {
       if (isalpha(c)) {
-         if (isupper(x)) {
-            x -= UPPER_OFFSET;
-         }
-         else if (islower(x)) {
-            x -= LOWER_OFFSET;
-         }
-         if (isupper(c)) {
-            c -= UPPER_OFFSET;
-         }
-         else if (islower(c)) {
-            c -= LOWER_OFFSET;
-         }
-         c = (c + x) % 26;
-         putchar(c + UPPER_OFFSET);
+         x = key[i++ % keylen];
+         c = toupper(c);
+         c -= OFFSET;
+         if (mode)
+            c = mod((c - x), ALPHABET_SIZE);
+         else
+            c = mod((c + x), ALPHABET_SIZE);
+         c += OFFSET;
       }
-      else {
-         putchar(c);
-      }
+      putchar(c);
    }
 
    if (infile)
