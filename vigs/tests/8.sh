b873d05dd7903e380aaf5da4f43a1f1d08049371
Max Zinkus <mzinkus@calpoly.edu>
2016-04-25 17:17:43 -0700

    Add large file test for ftable

---
+++ vigs/tests/8.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -f tests/big.in ]; then
+    size=$(du -hs tests/big.in | awk '{ print $1 }')
+    echo -n "Test 8: ftable $size....."
+    /usr/bin/time -f " (time elapsed: %es)" ./ftable tests/big.in >/dev/null 2>big.out
+    ret=$?
+    if [ $ret -ne 0 ]
+    then 
+        echo -ne "\033[31mFAIL\033[0m"
+        false
+    else
+        echo -ne "\033[32mPASS\033[0m"
+        true
+    fi
+    cat big.out
+    rm big.out
+else
+    echo "Skipping test 8: no tests/big.in"
+fi
