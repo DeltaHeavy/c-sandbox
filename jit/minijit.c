a834755d8d86f1275fdb1e3625887f1f804cf8f0
Max Zinkus <mzinkus@calpoly.edu>
2016-10-13 23:11:31 -0700

    Comment minijit

--- jit/minijit.c
+++ jit/minijit.c
@@ -11,9 +11,10 @@
 
 // https://cmcenroe.me/2016/10/13/babys-first-jit.html
 
-typedef int32_t (*fptr)(int32_t);
+typedef int32_t (*fptr)(int32_t); // define fptr to be a function int -> int
 
 int main(int argc, char **argv) {
+   // map one page
    uint32_t page = getpagesize();
    uint8_t *code = mmap(NULL, page, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
 
@@ -22,11 +23,12 @@ int main(int argc, char **argv) {
    } 
 
    if (argc < 2) {
-      err(EX_USAGE, "arguments");
+      err(EX_USAGE, "argument needed");
    }
 
    int32_t term = (int32_t)strtol(argv[1], NULL, 10);
 
+   // map assembled ASM into memory w/ bits of term aligned
    code[0] = 0x48;
    code[1] = 0xc7;
    code[2] = 0xc0;
@@ -39,13 +41,17 @@ int main(int argc, char **argv) {
    code[9] = 0xf8;
    code[10] = 0xc3;
 
+   // map read-execute
    int error = mprotect(code, page, PROT_READ | PROT_EXEC);
 
    if (error) {
       err(EX_OSERR, "mprotect");
    }
 
+   // cast to function (-Werror doesn't like this)
    fptr fn = (fptr)code;
+
+   // call with arguments
    printf("%d %d %d\n", fn(-1), fn(0), fn(1));
 
    return EX_OK;
